f7e07e32c0eb571f23a2bd92365bc21e80224b87
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2025-05-26 18:34:19 +0700

    Update lombok

--- payment-service/src/main/java/org/example/paymentservice/exception/AppException.java
+++ payment-service/src/main/java/org/example/paymentservice/exception/AppException.java
@@ -1,5 +1,10 @@
 package org.example.paymentservice.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class AppException extends RuntimeException {
 
     public AppException(ErrorCode errorCode) {
