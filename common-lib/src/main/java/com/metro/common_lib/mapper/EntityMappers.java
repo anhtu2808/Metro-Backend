6206836d7462936fa9a93cdd328fd0a7c877f82e
TranPhuKhag <trankhang0990@gmail.com>
2025-06-06 00:12:42 +0700

    Update EntityMappers

--- common-lib/src/main/java/com/metro/common_lib/mapper/EntityMappers.java
+++ common-lib/src/main/java/com/metro/common_lib/mapper/EntityMappers.java
@@ -3,9 +3,9 @@ package com.metro.common_lib.mapper;
 import java.util.List;
 
 public interface EntityMappers<E, CRE, UP, RP> {
-    E toEntity(CRE creationRequest);
-    E updateFrom(UP updateRequest);
+    E toEntity(CRE request);
+    void updateEntity(E entity, UP request);
     RP toResponse(E entity);
-    List<E> toEntity(List<CRE> creationRequestList);
+    List<E> toEntity(List<CRE> requestList);
     List<RP> toResponse(List<E> entityList);
 }
