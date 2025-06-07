90967fb1b7942bea9b688de275597802e63361c1
TranPhuKhag <trankhang0990@gmail.com>
2025-06-08 01:28:54 +0700

    [BE-0032] CRUD bus route

---
+++ route-service/src/main/java/com/metro/route/controller/StationController.java
@@ -0,0 +1,26 @@
+package com.metro.route.controller;
+
+import com.metro.common_lib.controller.AbstractController;
+import com.metro.common_lib.service.AbstractService;
+import com.metro.route.dto.request.station.StationCreationRequest;
+import com.metro.route.dto.request.station.StationUpdateRequest;
+import com.metro.route.dto.response.StationResponse;
+import com.metro.route.entity.Station;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stations")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class StationController extends AbstractController<
+        Station,
+        StationCreationRequest,
+        StationUpdateRequest,
+        StationResponse
+        > {
+    public StationController(AbstractService<Station, StationCreationRequest, StationUpdateRequest, StationResponse> service) {
+        super(service);
+    }
+}
--- route-service/src/main/java/com/metro/route/dto/request/station/StationUpdateRequest.java
+++ route-service/src/main/java/com/metro/route/dto/request/station/StationUpdateRequest.java
@@ -1,8 +1,11 @@
 package com.metro.route.dto.request.station;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 @NoArgsConstructor
@@ -12,14 +15,16 @@ import lombok.experimental.FieldDefaults;
 public class StationUpdateRequest {
 
     String stationCode;
-
+    @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters")
     String name;
 
+    @URL(message = "Image URL must be a valid URL")
+    @Size(max = 2048, message = "Image URL must not exceed 2048 characters")
     String imageUrl;
-
+    @Size(min = 1, max = 500, message = "Address must be between 1 and 500 characters")
     String address;
-
+    @Pattern(regexp = "^-?([1-8]?[0-9]|[1-9]0)\\.{1}\\d{1,6}$", message = "Invalid latitude format")
     String latitude;
-
+    @Pattern(regexp = "^-?((1[0-7]|[1-9])?[0-9]|180)\\.{1}\\d{1,6}$", message = "Invalid longitude format")
     String longitude;
 }
