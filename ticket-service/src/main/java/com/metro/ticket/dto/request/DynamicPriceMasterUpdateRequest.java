706e063f6402ed486adf5a9de87e60c8acdc8343
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2025-06-19 00:37:18 +0700

    [BE-0048] CRUD price master

--- ticket-service/src/main/java/com/metro/ticket/dto/request/DynamicPriceMasterUpdateRequest.java
+++ ticket-service/src/main/java/com/metro/ticket/dto/request/DynamicPriceMasterUpdateRequest.java
@@ -6,22 +6,18 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class DynamicPriceMasterRequest {
-    @NotNull(message = "Line ID is required")
+public class DynamicPriceMasterUpdateRequest {
+
     Long lineId;
-    @NotNull(message = "Start price is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Start price must be greater than 0")
     BigDecimal startPrice;
-    @NotNull(message = "Price per km is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Price per km must be greater than 0")
     BigDecimal pricePerKm;
-    @NotNull(message = "Start range is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Start range must be greater than 0")
     BigDecimal startRange;
 }
