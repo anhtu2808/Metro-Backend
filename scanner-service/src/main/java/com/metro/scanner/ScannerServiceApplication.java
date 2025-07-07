dd0ed7cdc0ffcfe5b6d42614c4f88f0c763c0b6e
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-07-07 22:27:36 +0700

    [BE-0037] Define interface scanner service

--- scanner-service/src/main/java/com/metro/scanner/ScannerServiceApplication.java
+++ scanner-service/src/main/java/com/metro/scanner/ScannerServiceApplication.java
@@ -2,7 +2,9 @@ package com.metro.scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ScannerServiceApplication {
 
--- scanner-service/src/main/java/com/metro/scanner/dto/response/ScannerResponse.java
+++ scanner-service/src/main/java/com/metro/scanner/dto/response/ScannerResponse.java
@@ -9,5 +9,5 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ScannerResponse {
-    Long id;
+    boolean isValidate;
 }
