ce1b83fe3f8438706875a2f57204a794fd34f6bd
Đặng Mai Anh Tú <tudmase182951@fpt.edu.vn>
2025-07-14 21:30:00 +0700

    [RF-0079] Refactor StationService Add API<StationResponse>

--- route-service/src/main/java/com/metro/route/controller/StationController.java
+++ route-service/src/main/java/com/metro/route/controller/StationController.java
@@ -1,5 +1,6 @@
 package com.metro.route.controller;
 
+import com.metro.common_lib.dto.response.ApiResponse;
 import com.metro.common_lib.dto.response.PageResponse;
 import com.metro.route.dto.request.station.StationCreationRequest;
 import com.metro.route.dto.request.station.StationUpdateRequest;
@@ -25,31 +26,55 @@ public class StationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public StationResponse createStation(@Valid @RequestBody StationCreationRequest request) {
-        return stationService.createStation(request);
+    public ApiResponse<StationResponse> createStation(@Valid @RequestBody StationCreationRequest request) {
+        return ApiResponse.<StationResponse>builder()
+                .code(HttpStatus.CREATED.value())
+                .message("Station created successfully")
+                .result(stationService.createStation(request))
+                .build();
     }
 
     @GetMapping("/{id}")
-    public StationResponse getStationById(@PathVariable Long id) {
-        return stationService.getStationById(id);
+    public ApiResponse<StationResponse> getStationById(@PathVariable Long id) {
+        return ApiResponse.<StationResponse>builder()
+                .code(HttpStatus.OK.value())
+                .message("Station fetched successfully")
+                .result(stationService.getStationById(id))
+                .build();
     }
 
     @GetMapping
-    public PageResponse<StationResponse> getStations(@RequestParam(defaultValue = "0") int page,
-                                                     @RequestParam(defaultValue = "10") int size,
-                                                     @RequestParam(defaultValue = "id") String sort) {
-        return stationService.getStations(page, size, sort);
+    public ApiResponse<PageResponse<StationResponse>> getStations(@RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "10") int size,
+                                                                  @RequestParam(defaultValue = "id") String sort
+    ) {
+        PageResponse<StationResponse> pageResp = stationService.getStations(page, size, sort);
+        return ApiResponse.<PageResponse<StationResponse>>builder()
+                .code(HttpStatus.OK.value())
+                .message("Stations fetched successfully")
+                .result(pageResp)
+                .build();
     }
 
     @PutMapping("/{id}")
-    public StationResponse updateStation(@PathVariable Long id, @Valid @RequestBody StationUpdateRequest request) {
-        return stationService.updateStation(id, request);
+    public ApiResponse<StationResponse> updateStation(
+            @PathVariable Long id,
+            @Valid @RequestBody StationUpdateRequest request
+    ) {
+        return ApiResponse.<StationResponse>builder()
+                .code(HttpStatus.OK.value())
+                .message("Station updated successfully")
+                .result(stationService.updateStation(id, request))
+                .build();
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteStation(@PathVariable Long id) {
+    public ApiResponse<Void> deleteStation(@PathVariable Long id) {
         stationService.deleteStation(id);
+        return ApiResponse.<Void>builder()
+                .code(HttpStatus.NO_CONTENT.value())
+                .message("Station deleted successfully")
+                .build();
     }
-
 }
