6fbc1aa6744a41e3ae1325436e97e8dadb5ba133
Trinh Nhan <147188783+trinhhnhann@users.noreply.github.com>
2025-07-15 16:23:33 +0700

    [RF-0073] Refactor ContentService

--- content-service/src/main/java/com/metro/content/service/ContentService.java
+++ content-service/src/main/java/com/metro/content/service/ContentService.java
@@ -8,17 +8,17 @@ import com.metro.content.enums.ContentType;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ContentService {
-//    @PreAuthorize("hasAuthority('CONTENT_CREATE')")
+    @PreAuthorize("hasAuthority('CONTENT_CREATE')")
     ContentResponse create(ContentRequest request);
 
-//    @PreAuthorize("hasAuthority('CONTENT_UPDATE')")
+    @PreAuthorize("hasAuthority('CONTENT_UPDATE')")
     ContentResponse update(Long id, ContentUpdateRequest request);
 
     ContentResponse findById(Long id);
 
     PageResponse<ContentResponse> findAll(int page, int size, String arrange);
 
-//    @PreAuthorize("hasAuthority('CONTENT_DELETE')")
+    @PreAuthorize("hasAuthority('CONTENT_DELETE')")
     void delete(Long id);
 
     PageResponse<ContentResponse> getContentByType(ContentType type, int page, int size);
