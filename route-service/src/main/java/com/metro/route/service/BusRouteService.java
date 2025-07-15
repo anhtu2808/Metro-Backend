94dd038f52e9e1db0376a86b8b713876d3e0a6d8
TranPhuKhag <trankhang0990@gmail.com>
2025-07-16 01:24:10 +0700

    re-enable PreAuthorize on BusRouteService methods

--- route-service/src/main/java/com/metro/route/service/BusRouteService.java
+++ route-service/src/main/java/com/metro/route/service/BusRouteService.java
@@ -8,17 +8,17 @@ import com.metro.route.dto.response.BusRouteResponse;
 
 public interface BusRouteService {
 
-//    @PreAuthorize("hasAuthority('BUS_ROUTE_CREATE')")
+    @PreAuthorize("hasAuthority('BUS_ROUTE_CREATE')")
     BusRouteResponse createBusRoute(BusRouteCreationRequest request);
 
     BusRouteResponse getBusRouteById(Long id);
 
     PageResponse<BusRouteResponse> getBusRoutes(int page, int size, String sort);
 
-//    @PreAuthorize("hasAuthority('BUS_ROUTE_UPDATE')")
+    @PreAuthorize("hasAuthority('BUS_ROUTE_UPDATE')")
     BusRouteResponse updateBusRoute(Long id, BusRouteUpdateRequest request);
 
-//    @PreAuthorize("hasAuthority('BUS_ROUTE_DELETE')")
+    @PreAuthorize("hasAuthority('BUS_ROUTE_DELETE')")
     void deleteBusRoute(Long id);
 
     PageResponse<BusRouteResponse> getBusRoutesByStationId(Long stationId, int page, int size, String sort);
